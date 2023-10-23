a54c9836cae760128b28d022b9ef2c7aebbcd9b7
Sainith123 <yamsanisainith@gmail.com>
2023-10-23 17:12:03 +0530

    new feats

---
+++ README.md
@@ -0,0 +1,29 @@
+#  React Quiz App 
+
+A Simple React Quiz App which contains the 7 MCQ’s which have 4 options in each.
+
+
+
+### Version
+1.0.0
+
+## 📝 Usage
+
+### Clone this Repo
+```
+https://github.com/Sainith123/Sainith-quiz-app.git
+```
+### Installation
+
+```sh
+$ npm install
+```
+
+### ✔️ Run
+
+```sh
+$ npm start
+```
+
+Then go to http://localhost:8000
+
---
+++ src/components/questions_data/data.js
@@ -0,0 +1,150 @@
+export default [
+    {
+        question: "What is the virtual DOM in React?",
+        answers: [
+            "A browser feature that React uses for performance optimization.",
+            "A lightweight representation of the actual DOM in memory.",
+            "A special type of DOM used for virtual reality applications.",
+            "An experimental feature for rendering React components."
+        ],
+        correct: 1
+    }
+    ,
+    {
+        question: "What is the main purpose of React Router?",
+        answers: [
+            "To manage the state of React components.",
+            "To create animations in React applications.",
+            "To handle routing and navigation in single-page applications.",
+            "To organize the folder structure of a React project."
+        ],
+        correct: 2
+    }
+    ,
+    {
+        question: "In React, what is the significance of the 'key' prop when rendering a list of elements?",
+        answers: [
+            "It specifies the color of the elements.",
+            "It is used to uniquely identify elements when rendering lists.",
+            "It sets the font size for the list items.",
+            "It determines the position of the elements on the screen."
+        ],
+        correct: 1
+    }
+    ,    
+
+    {
+        question: 'JSX is typesafe?',
+        answers: [
+            'True',
+            'False',
+            'None of the Above',
+            'Cannot Be Determined'
+        ],
+        correct: 1
+    },    
+    {
+        question: 'React merges the object you provide into the current state using which of the following',
+        answers: [
+            'setState()',
+            'State()', 
+            'getState()', 
+            'None of the Above'
+        ],    
+        correct: 1
+    }, 
+    {
+        question: "What lifecycle method is used for performing side effects in a React component?",
+        answers: [
+            "componentWillUnmount",
+            "componentWillUpdate",
+            "componentDidMount",
+            "componentDidUpdate"
+        ],
+        correct: 3
+    },
+       
+    {
+        question: 'Arbitrary inputs of components are called?',
+        answers: [
+            'Keys',
+            'Props', 
+            'Elements', 
+            'Ref'
+        ],    
+        correct: 2
+    },
+    {
+        question: "What is the purpose of the 'useState' hook in React?",
+        answers: [
+            "To manage component routing.",
+            "To fetch data from an API.",
+            "To manage and update state in functional components.",
+            "To style components using CSS."
+        ],
+        correct: 2
+    }
+    ,
+    {
+        question: "What is a controlled component in React?",
+        answers: [
+            "A component that is uncontrollable and cannot be manipulated.",
+            "A component that manages its own state internally.",
+            "A component that relies on external state management.",
+            "A component that is restricted from rendering in certain contexts."
+        ],
+        correct: 2
+    },
+    {
+        question: "What is the purpose of the 'map' function in React when rendering lists of elements?",
+        answers: [
+            "To apply a function to each element in an array and return a new array.",
+            "To sort the elements in the list.",
+            "To filter out certain elements from the list.",
+            "To reverse the order of the elements."
+        ],
+        correct: 0
+    }
+    ,{
+        question: "What is React context used for?",
+        answers: [
+            "Styling components with CSS.",
+            "Sharing data between components without explicitly passing it through props.",
+            "Managing routing and navigation.",
+            "Creating animations in React applications."
+        ],
+        correct: 1
+    }
+        ,
+    
+    {
+        question: '_____ can be done while more than one element needs to be returned from a component?',
+        answers: [
+            'Abstraction',
+            'Packing', 
+            'Insulation', 
+            'Wrapping'
+        ],    
+        correct: 4
+    },
+    {
+        question: 'Which of the following needs to be updated to achieve dynamic UI updates?',
+        answers: [
+            'State',
+            'Props', 
+            'Components', 
+            'None of the Above'
+        ],    
+        correct: 1
+    },
+    {
+        question: 'Lifecycle methods are mainly used _____',
+        answers: [
+            'To keep track of event history',
+            'To enhance components', 
+            'Free up resources', 
+            'None of the Above'
+        ],    
+        correct: 3
+    }
+]
